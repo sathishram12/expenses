87b20d85ac8b81ccb183d2d7d07b72eafd2cdaf0
Sathish Ram <mailmesathish.m@gmail.com>
2021-06-29 20:44:01 +0530

    removed cloud push and githubpage changes

--- deploy.sh
+++ deploy.sh
@@ -9,23 +9,3 @@ fi
 git add .
 git commit -m "$1"
 git push origin main
-
-# Todo in future this will deploy in cloud
-REACT_APP_SHEET_ID=1WDvmmWrJ5uxlFGiQISlpzMCBn4UOn3zBB0yEcTSOcr0 npm run build
-echo "I am here"
-cp -a build/. production/
-cd production
-git add .
-git commit -m "$1"
-git push origin production
-cd ..
-
-# github pages
-
-npm run build
-cp -a build/. demo/
-cd demo
-git add .
-git commit -m "$1"
-git push origin gh-pages
-cd ..
