5a9906f7768547bf7064fa94cc59f0fda32da6d2
Sathish Ram <mailmesathish.m@gmail.com>
2021-07-14 21:31:07 +0530

    production image

--- src/components/index.js
+++
@@ -1,9 +0,0 @@
-import ExpenseList from "./expense-list/ExpenseList";
-import ExpenseForm from "./expense-form/ExpenseForm";
-import LoadingBar from "./LoadingBar";
-
-export {
-  ExpenseList,
-  ExpenseForm,
-  LoadingBar
-};
